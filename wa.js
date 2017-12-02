cf2319069eda562ac154cf3eeb2f093d17809521
Aditya Kumar <sankmr76@gmail.com>
2017-12-02 21:54:11 +0530

    notification repeat error fixed

--- wa.js
+++ wa.js
@@ -1,21 +1,9 @@
-var not=[];
-var stk=[];
-
-function data(){
-  var nn=notify.value;
-  var st=stock.value;
-  not.push(nn);
-  stk.push(st);
-}
-
+var flag=true;
 function adi(){
-  for(var x=0;x<st.length;x++){
-    var num=stk[x]+"@c.us";
     var len=Store.Chat.models.length;
-    //var num="917275493769@c.us";
+    var num="917275493769@c.us";
     var ans;
-    var flag=true;
-    Store.Chat.gadd(num);
+    //var flag=true;
     for(var i=0;i<len;i++){
       var n=Store.Chat.models[i].__x_id;
       if(n===num){
@@ -27,16 +15,10 @@ function adi(){
   //console.log(len);
   if(Store.Chat.models[ans].__x_presence.__x_isOnline&&flag){
     console.log(Store.Chat.models[ans].__x_id+" is Online");
-    //send message function
-
-
     }
-  if(Store.Chat.models[ans].__x_presence.__x_isOnline){
+  if(Store.Chat.models[ans].__x_presence.__x_isOnline)
     flag=false;
-    }
-  else if(!Store.Chat.models[ans].__x_presence.__x_isOnline){
+  else
     flag=true;
-    }
-  }
 }
 setInterval(adi, 10000);
