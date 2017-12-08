ff26aa74f643aad3e0c389e47d0e064fdfbc63df
Shubham Singh <mynameshubham@gmail.com>
2017-12-08 18:38:39 +0400

    databinding done for both phone numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    title = 'app';
+    notify: number;
+    stalk: number;
+    check1: boolean = true;
+    check2: boolean = true;
+    check3: boolean = true;
+    check4: boolean = true;
+
+    public constructor() {
+        
+    }
+
+    checkbox1_onClick() {
+        this.check1 = !this.check1;
+    }
+    checkbox2_onClick() {
+        this.check2 = !this.check2;
+    }
+    checkbox3_onClick() {
+        this.check3 = !this.check3;
+    }
+    checkbox4_onClick() {
+        this.check4 = !this.check4;
+    }
+
+    monitor(junk1, junk2) {
+        console.log({ junk1, junk2});
+    }
+
+
+    ngOnInit() {
+
+    }
 }
